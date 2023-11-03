0a1577eaef3fc8f268077bbdd17002de23bd8c67
Andrengineer <ndrengineer@gmail.com>
2023-11-02 20:45:49 -0500

    Fix namespaces

--- index.php
+++ index.php
@@ -2,9 +2,3 @@
 
 // Inicializar configuraciones
 include_once('config' . DIRECTORY_SEPARATOR . 'init.php');
-
-
-require('../CMS/vendor/autoload.php');
-
-$dotenv = Dotenv\Dotenv::createImmutable('../');
-$dotenv->load();
