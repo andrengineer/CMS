9c27c472e48f45fdff0c4f9b52d4efa65997f804
Andrengineer <ndrengineer@gmail.com>
2023-11-02 20:33:12 -0500

    Composer delete

--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 
 // Inicializar configuraciones
 include_once('config' . DIRECTORY_SEPARATOR . 'init.php');
+
+
+require('../CMS/vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->load();
